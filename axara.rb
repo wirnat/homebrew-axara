2cf33a1caf44ded4d3ace2bb20cc766f6ca8799e
Wiranatha <35485901+wirnat@users.noreply.github.com>
2023-04-19 22:59:01 +0800

    Update axara.rb

--- axara.rb
+++ axara.rb
@@ -2,7 +2,7 @@ class Axara < Formula
   desc "CLI for Code Generator"
   homepage "https://github.com/wirnat/axara"
   url "https://github.com/wirnat/axara/archive/refs/tags/v1.2.0.tar.gz" # Ganti URL dengan URL tautan unduhan package Anda
-  sha256 "HASH_SHA256_FILE" # Ganti HASH_SHA256_FILE dengan hash SHA256 dari file unduhan Anda
+  sha256 "6d7b66d824347d48cf92ad9006cc7b6e1c04aca9dd780af00dfb336214938fff" # Ganti HASH_SHA256_FILE dengan hash SHA256 dari file unduhan Anda
   version "1.0.0"
 
   def install
